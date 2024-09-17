c5d9c593b01c6569220fe858de7e2fada20b6a88
Profo <profogitrepos@gmail.com>
2024-09-17 15:57:13 +0300

    fixed the checking user input if empty and the length of the user value greater than 2

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
-import java.util.List;
+
 import java.util.Scanner;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import tools.*;
 import tools.Personal.Gender;
@@ -12,24 +11,52 @@ public class App {
         String firstname = "";
         String lastname = "";
         String email = "";
-        String password = "";
         String jobField = "";
         String contact = "";
-        double height = 0;
+        double height = 0.1;
+        String password = "";
         // gender var
 
         Introduce.introduce();
 
         // initialize the user input -------------------------------------
+        // ================================================================
         System.out.print("To proceed, please enter your first name: ");
         Scanner f_name = new Scanner(System.in);
-        firstname = cap(f_name.nextLine());
-        f_name.close();
+        firstname = cap(emptyChecker(f_name.nextLine()));
+        // ================================================================
+
+        System.out.print("Enter your last name: ");
+        Scanner l_name = new Scanner(System.in);
+        lastname = cap(emptyChecker(l_name.nextLine()));
+        // ================================================================
+
+        System.out.print("Enter your email address: ");
+        Scanner emailID = new Scanner(System.in);
+        email = emptyChecker(emailID.nextLine());
+        // ================================================================
+
+        System.out.print("Enter your field of specification: ");
+        Scanner job = new Scanner(System.in);
+        jobField = emptyChecker(job.nextLine());
+        // ================================================================
+
+        System.out.print("Enter your height: ");
+        Scanner heightF = new Scanner(System.in);
+        height = heightF.nextDouble();
+        // ================================================================
+
+        System.out.print("Enter your password: ");
+        Scanner pwd = new Scanner(System.in);
+        password = emptyChecker(pwd.nextLine());
+        // ================================================================
 
         Personal person = new Personal(firstname, lastname, email, contact, height, password, jobField,
                 Status.SINGLE,
                 Gender.MALE);
 
+        // close the scanners
+        f_name.close(); l_name.close(); emailID.close(); heightF.close();job.close(); pwd.close();
         System.out.println(person);
     }
 
@@ -38,19 +65,7 @@ public class App {
     // capitalize and check for empty string =======
     public static String cap(String val) {
         String firstLetter = "", strNoHead = "";
-
-        emptyChecker(val);
-        // sc.close();
-
-        while (val.length() < 2) {
-            System.out.print("Input length cannot be less than 2: ");
-            Scanner scr = new Scanner(System.in);
-            val = scr.next();
-
-            emptyChecker(val);
-        }
         if (val.length() > 1) {
-
             // extract the first letter and convert to uppercase
             firstLetter = val.substring(0, 1).toUpperCase();
 
@@ -62,13 +77,35 @@ public class App {
 
     }
 
-    //empty value checker
+    // empty value checker
     public static String emptyChecker(String value) {
-        while (value.isEmpty()) {
-            System.out.print("The input cannot be empty: ");
-            Scanner sc = new Scanner(System.in);
-            value = sc.nextLine();
-        }
+
+        // check if value is empty
+        if (value.isEmpty())
+            while (value.isEmpty()) {
+                System.out.print("The input cannot be empty: ");
+                @SuppressWarnings("resource")
+                Scanner sc = new Scanner(System.in);
+                value = sc.nextLine();
+            }
+            if (value.length() < 2){
+                // check value length
+                while (value.length() < 2) {
+                    System.out.print("Input length cannot be less than 2: ");
+                    @SuppressWarnings("resource")
+                    Scanner scr = new Scanner(System.in);
+                    value = scr.nextLine();
+                    
+                    //  check if value is empty
+                    if (value.isEmpty())
+                        while (value.isEmpty()) {
+                            System.out.print("The input cannot be empty: ");
+                            @SuppressWarnings("resource")
+                            Scanner sc = new Scanner(System.in);
+                            value = sc.nextLine();
+                    }
+                }
+            }
         return value;
     }
 
